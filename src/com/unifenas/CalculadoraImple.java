78d7f9e6d306cc98b637856029fb195315ac0daa
Gustavo Ribeiro <gustavorc01@hotmail.com>
2019-10-31 16:28:23 -0300

    CalculadoraImple changes

--- src/com/unifenas/CalculadoraImple.java
+++ src/com/unifenas/CalculadoraImple.java
@@ -1,4 +1,16 @@
 package com.unifenas;
 
-public class CalculadoraImple {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CalculadoraImple extends UnicastRemoteObject implements  Calculadora {
+
+
+    protected CalculadoraImple() throws RemoteException {
+    }
+
+    @Override
+    public long add(long a, long b) throws RemoteException {
+        return 0;
+    }
 }
