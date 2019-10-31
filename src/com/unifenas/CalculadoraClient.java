4ab19db8abab32d3abe87c2f50d0b60d7ec028b8
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-10-31 16:38:53 -0300

    Client

--- src/com/unifenas/CalculadoraClient.java
+++ src/com/unifenas/CalculadoraClient.java
@@ -1,8 +1,15 @@
 package com.unifenas;
 
+import java.rmi.Naming;
+
 public class CalculadoraClient {
     public static void main(String[] args){
-
+        try{
+            Calculadora c = (Calculadora) Naming.lookup("rmi://172.16.50.45:1099/CalculadoraService");
+            System.out.println("Adição : " + c.add(10,15));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
--- src/com/unifenas/CalculadoraImple.java
+++ src/com/unifenas/CalculadoraImple.java
@@ -4,12 +4,12 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class CalculadoraImple extends UnicastRemoteObject implements  Calculadora {
-
+    private static final long serialVersionUID = 1L;
 
     protected CalculadoraImple() throws RemoteException {
+        super();
     }
 
-    @Override
     public long add(long a, long b) throws RemoteException {
         return a + b;
     }
