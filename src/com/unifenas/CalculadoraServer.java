28164e72ac32856bab423243c2702f2e0877c1d0
Gustavo Ribeiro <gustavorc01@hotmail.com>
2019-10-31 16:41:01 -0300

    CalculadoraServer

--- src/com/unifenas/CalculadoraServer.java
+++ src/com/unifenas/CalculadoraServer.java
@@ -1,4 +1,24 @@
 package com.unifenas;
 
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+
 public class CalculadoraServer {
+
+    CalculadoraServer(){
+        try{
+            System.setProperty("java.rmi.server.hostname", "172.16.50.45");
+            LocateRegistry.createRegistry(1099);
+            Calculadora c = new CalculadoraImple();
+            Naming.bind("CalculadoraService", (Remote) c);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new CalculadoraServer();
+    }
 }
