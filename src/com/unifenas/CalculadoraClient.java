2691b91646652764ccc9374c3a543867cab5fa1d
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-11-04 20:53:16 -0300

    ...

--- src/com/unifenas/CalculadoraClient.java
+++ src/com/unifenas/CalculadoraClient.java
@@ -10,46 +10,54 @@ public class CalculadoraClient {
 
             Scanner sc = new Scanner(System.in);
 
-            int opcao = 0;
+            Boolean continuar = true;
 
-            do {
-                System.out.println("----------MENU----------");
-                System.out.println("[1] Soma");
-                System.out.println("[2] Subtração");
-                System.out.println("[3] Multiplicação");
-                System.out.println("[4] Divisão");
-                System.out.println("------------------------");
-                System.out.println("Escolha uma opção: ");
-                opcao = sc.nextInt();
-                System.out.println("------------------------");
+            while (continuar){
+                int opcao = 0;
 
-                if(opcao < 1 || opcao > 4){
-                    System.out.println("Não existe esta opção, escolha um nº entre 1 à 4");
-                    System.out.println("");
+                do {
+                    System.out.println("----------MENU----------");
+                    System.out.println("[1] Soma");
+                    System.out.println("[2] Subtração");
+                    System.out.println("[3] Multiplicação");
+                    System.out.println("[4] Divisão");
+                    System.out.println("------------------------");
+                    System.out.println("Escolha uma opção: ");
+                    opcao = sc.nextInt();
+                    System.out.println("------------------------");
+
+                    if(opcao < 1 || opcao > 4){
+                        System.out.println("Não existe esta opção, escolha um nº entre 1 à 4");
+                        System.out.println("");
+                    }
+
+                }while (opcao < 1 || opcao > 4);
+
+
+                System.out.println("Digite o 1º nº:");
+                float num1 = sc.nextFloat();
+                System.out.println("Digite o 2º nº:");
+                float num2 = sc.nextFloat();
+
+                switch (opcao){
+                    case 1:
+                        System.out.println("Soma: " + c.add(num1, num2));
+                        break;
+                    case 2:
+                        System.out.println("Subtração: " + c.subtract(num1, num2));
+                        break;
+                    case 3:
+                        System.out.println("Multiplicação: " + c.multiply(num1, num2));
+                        break;
+                    case 4:
+                        System.out.println("Divisão: " + c.Division(num1, num2));
+                        break;
+                    case 5:
+                        continuar = false;
+                        break;
                 }
-
-            }while (opcao < 1 || opcao > 4);
-
-
-            System.out.println("Digite o 1º nº:");
-            float num1 = sc.nextFloat();
-            System.out.println("Digite o 2º nº:");
-            float num2 = sc.nextFloat();
-
-            switch (opcao){
-                case 1:
-                    System.out.println("Soma: " + c.add(num1, num2));
-                    break;
-                case 2:
-                    System.out.println("Subtração: " + c.subtract(num1, num2));
-                    break;
-                case 3:
-                    System.out.println("Multiplicação: " + c.multiply(num1, num2));
-                    break;
-                case 4:
-                    System.out.println("Divisão: " + c.Division(num1, num2));
-                    break;
             }
+
         } catch (Exception e){
             e.printStackTrace();
         }
